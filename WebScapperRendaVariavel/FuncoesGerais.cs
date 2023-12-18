aaea5a2868c36eb687ed8f6e49d9ebddc3df226b
iaguuu <iagooliveiraviana@gmail.com>
2023-12-18 16:10:55 -0300

    first commit

---
+++ WebScapperRendaVariavel/FuncoesGerais.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Data;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace WebScapperRendaVariavel
+{
+    internal class FuncoesGerais
+    {
+        public string encodeBase64(string value)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(value));
+        }
+        public string decodeBase64(string value)
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(value));
+        }
+        private static readonly Random random = new Random();
+        public string randomString(int OpcionalLength = 0)
+        {
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            char[] stringChars = new char[(OpcionalLength == 0) ? random.Next(0, 1000) : OpcionalLength];
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+            return new string(stringChars);
+        }
+
+        public string mWebRequest(string pUrl, string pMethod, string pUserAgent = ".NET Framework project", WebHeaderCollection pHeaders = null)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(pUrl);
+                request.Method = pMethod;
+                request.UserAgent = pUserAgent;
+
+                if (pHeaders != null) { request.Headers = pHeaders; }
+
+                using (HttpWebResponse iRequestResponse = (HttpWebResponse)request.GetResponse())
+                {
+                    using (StreamReader iResposeStreamReader = new StreamReader(iRequestResponse.GetResponseStream(), Encoding.UTF8))
+                    {
+                        return iResposeStreamReader.ReadToEnd();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"Erro: {ex.Message}";
+            }
+        }
+
+        public DataTable mJsonToTable(string pJson)
+        {
+            DataTable iDataTable = JsonConvert.DeserializeObject<DataTable>(pJson);
+            return iDataTable;
+        }
+
+    }
+}
---
+++ WebScapperRendaVariavel/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScapperRendaVariavel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine(funcoes.randomString() + "\n");
+            // Console.WriteLine(funcoes.randomString() + "\n");
+
+            //string result = mWebRequest("https://sistemaswebb3-listados.b3.com.br/fundsProxy/fundsCall/GetListFundDownload/eyJ0eXBlRnVuZCI6NywicGFnZU51bWJlciI6MSwicGFnZVNpemUiOjIwfQ==", "GET");
+            //string result = mWebRequest("https://sistemaswebb3-listados.b3.com.br/fundsProxy/fundsCall/GetListFundDownload/eyJ0eXBlRnVuZCI6MjcsInBhZ2VOdW1iZXIiOjEsInBhZ2VTaXplIjoyMH0=", "GET");
+            //string result = mWebRequest("https://sistemaswebb3-listados.b3.com.br/fundsProxy/fundsCall/GetListFundDownload/eyJ0eXBlRnVuZCI6MzQsInBhZ2VOdW1iZXIiOjEsInBhZ2VTaXplIjoyMH0=", "GET");
+
+            //Console.WriteLine(funcoes.decodeBase64(result.Replace(@"""", "")));
+
+
+            ////investidor10
+            //string result = mWebRequest("https://investidor10.com.br/fiis/rura11/", "GET");
+
+
+            ////fundsexplorer
+            //WebHeaderCollection customHeaders = new WebHeaderCollection();
+            //customHeaders.Add("x-funds-nonce", "61495f60b533cc40ad822e054998a3190ea9bca0d94791a1da");
+            //// customHeaders.Add("Alt-Used", "www.fundsexplorer.com.br");
+            //string result = funcoes.mWebRequest("https://www.fundsexplorer.com.br/wp-json/funds/v1/get-ranking", "GET", pHeaders: customHeaders);
+            //mProcessarFundos(result);
+
+
+            ////fundamentus
+            //string result2 = funcoes.mWebRequest("https://www.fundamentus.com.br/resultado.php", "GET");
+            //mProcessarAcoes(result2);
+            //Console.WriteLine(result2);
+        }
+
+    }
+}
