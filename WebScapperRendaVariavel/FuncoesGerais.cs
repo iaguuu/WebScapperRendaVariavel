e8575cb11425d17f181b646f6d5bfce5178b1dac
iaguuu <iagooliveiraviana@gmail.com>
2023-12-20 17:51:42 -0300

    Adding initial html parser

--- WebScapperRendaVariavel/FuncoesGerais.cs
+++ WebScapperRendaVariavel/FuncoesGerais.cs
@@ -29,15 +29,14 @@ namespace WebScapperRendaVariavel
             return new string(stringChars);
         }
 
-        public string mWebRequest(string pUrl, string pMethod, string pUserAgent = ".NET Framework project", WebHeaderCollection pHeaders = null)
+        public string mWebRequest(string Url, string Method, string UserAgent = ".NET Framework project", WebHeaderCollection Headers = null)
         {
             try
             {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(pUrl);
-                request.Method = pMethod;
-                request.UserAgent = pUserAgent;
-
-                if (pHeaders != null) { request.Headers = pHeaders; }
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+                request.Method = Method;
+                request.UserAgent = UserAgent;
+                if (Headers != null) { request.Headers = Headers; }
 
                 using (HttpWebResponse iRequestResponse = (HttpWebResponse)request.GetResponse())
                 {
@@ -53,11 +52,36 @@ namespace WebScapperRendaVariavel
             }
         }
 
-        public DataTable mJsonToTable(string pJson)
+        public DataTable JsonToTable(string Json)
         {
-            DataTable iDataTable = JsonConvert.DeserializeObject<DataTable>(pJson);
+            DataTable iDataTable = JsonConvert.DeserializeObject<DataTable>(Json);
             return iDataTable;
         }
+
+        public DataTable ConvertCsvStringToDataTable(bool isFilePath, string CSVContent, char CharDelimiter)
+        {
+            string[] Lines;
+            if (isFilePath) { Lines = File.ReadAllLines(CSVContent); } else { Lines = CSVContent.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries); }
+            string[] Fields = Lines[0].Split(new char[] { CharDelimiter });
+            int Cols = Fields.GetLength(0);
+            DataTable dt = new DataTable();
+            //1st row must be column names; force lower case to ensure matching later on.
+            for (int i = 0; i < Cols; i++)
+            {
+                dt.Columns.Add(Fields[i].ToLower(), typeof(string));
+            }
+            DataRow Row;
+            for (int i = 1; i < Lines.GetLength(0); i++)
+            {
+                Fields = Lines[i].Split(new char[] { CharDelimiter });
+                Row = dt.NewRow();
+                for (int f = 0; f < Cols; f++)
+                    Row[f] = Fields[f];
+                dt.Rows.Add(Row);
+            }
+            return dt;
+        }
+
 
     }
 }
--- WebScapperRendaVariavel/Program.cs
+++ WebScapperRendaVariavel/Program.cs
@@ -1,42 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using Newtonsoft.Json;
+using System.Security.Policy;
+using HtmlAgilityPack;
+using System.Xml.Linq;
 
 namespace WebScapperRendaVariavel
 {
+    public enum tiposAtivo { ACAO, FII, BDRS, rendaFixa };
     internal class Program
     {
+        private static FuncoesGerais funcoes = new FuncoesGerais();
         static void Main(string[] args)
         {
-            // Console.WriteLine(funcoes.randomString() + "\n");
-            // Console.WriteLine(funcoes.randomString() + "\n");
 
-            //string result = mWebRequest("https://sistemaswebb3-listados.b3.com.br/fundsProxy/fundsCall/GetListFundDownload/eyJ0eXBlRnVuZCI6NywicGFnZU51bWJlciI6MSwicGFnZVNpemUiOjIwfQ==", "GET");
-            //string result = mWebRequest("https://sistemaswebb3-listados.b3.com.br/fundsProxy/fundsCall/GetListFundDownload/eyJ0eXBlRnVuZCI6MjcsInBhZ2VOdW1iZXIiOjEsInBhZ2VTaXplIjoyMH0=", "GET");
-            //string result = mWebRequest("https://sistemaswebb3-listados.b3.com.br/fundsProxy/fundsCall/GetListFundDownload/eyJ0eXBlRnVuZCI6MzQsInBhZ2VOdW1iZXIiOjEsInBhZ2VTaXplIjoyMH0=", "GET");
+           ConsultaFundamentus();
+            
+            //ListarFiis();
 
-            //Console.WriteLine(funcoes.decodeBase64(result.Replace(@"""", "")));
+            //.Replace(@"""", "")
+            //  Thread thread1 = new Thread(() => { ListarFiis(); });
+            //  Thread thread2 = new Thread(() => { ListarFiis(); });
+            //  Thread thread3 = new Thread(() => { ListarFiis(); });
 
+        } 
+        protected static void ListarFiis()
+        {
+            string[] fundUrls = {
+                "https://sistemaswebb3-listados.b3.com.br/fundsProxy/fundsCall/GetListFundDownload/eyJ0eXBlRnVuZCI6NywicGFnZU51bWJlciI6MSwicGFnZVNpemUiOjIwfQ==",
+                "https://sistemaswebb3-listados.b3.com.br/fundsProxy/fundsCall/GetListFundDownload/eyJ0eXBlRnVuZCI6MjcsInBhZ2VOdW1iZXIiOjEsInBhZ2VTaXplIjoyMH0=",
+                "https://sistemaswebb3-listados.b3.com.br/fundsProxy/fundsCall/GetListFundDownload/eyJ0eXBlRnVuZCI6MzQsInBhZ2VOdW1iZXIiOjEsInBhZ2VTaXplIjoyMH0="
+            };
 
-            ////investidor10
-            //string result = mWebRequest("https://investidor10.com.br/fiis/rura11/", "GET");
+            DataTable mergedTable = new DataTable();
 
+            foreach (string fundUrl in fundUrls)
+            {
+                string requestResult = funcoes.mWebRequest(fundUrl, "GET");
+                string plainText = funcoes.decodeBase64(requestResult.Trim('"'));
+                DataTable fundTable = funcoes.ConvertCsvStringToDataTable(false, plainText, ';');
+                mergedTable.Merge(fundTable);
+            }
 
-            ////fundsexplorer
-            //WebHeaderCollection customHeaders = new WebHeaderCollection();
-            //customHeaders.Add("x-funds-nonce", "61495f60b533cc40ad822e054998a3190ea9bca0d94791a1da");
-            //// customHeaders.Add("Alt-Used", "www.fundsexplorer.com.br");
-            //string result = funcoes.mWebRequest("https://www.fundsexplorer.com.br/wp-json/funds/v1/get-ranking", "GET", pHeaders: customHeaders);
-            //mProcessarFundos(result);
-
-
-            ////fundamentus
-            //string result2 = funcoes.mWebRequest("https://www.fundamentus.com.br/resultado.php", "GET");
-            //mProcessarAcoes(result2);
-            //Console.WriteLine(result2);
         }
+        protected static DataTable ConsultaFundamentus()
+        {
+            string requestResult = funcoes.mWebRequest("https://www.fundamentus.com.br/resultado.php", "GET").Replace("\\", "").Trim('"');
+            DataTable requestResultDataTable = ParseHtmlTableToDataTable(requestResult, "//*[@id=\"resultado\"]");
+            return requestResultDataTable;
+        }
+        protected static DataTable ConsultaInvestidor10(tiposAtivo tipo, string ticker)
+        {
+            string requestResult = funcoes.mWebRequest($"https://investidor10.com.br/{tipo}/{ticker}/", "GET");
+            DataTable requestResultDataTable = funcoes.JsonToTable(requestResult.Replace("\\", "").Trim('"'));
+            return requestResultDataTable;
+        }
+        protected static DataTable ConsultaFundsExplorer()
+        {
+            WebHeaderCollection customHeaders = new WebHeaderCollection { { "x-funds-nonce", "61495f60b533cc40ad822e054998a3190ea9bca0d94791a1da" } };
+            string requestResult = funcoes.mWebRequest("https://www.fundsexplorer.com.br/wp-json/funds/v1/get-ranking", "GET", Headers: customHeaders);
+            DataTable requestResultDataTable = funcoes.JsonToTable(requestResult.Replace("\\", "").Trim('"'));
+            return requestResultDataTable;
+        }       
 
+        protected static DataTable ParseHtmlTableToDataTable(string html, string attributePath)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+            HtmlNodeCollection tableHeaders = doc.DocumentNode.SelectNodes($"{attributePath}//tr/th");
+            HtmlNodeCollection tableRows = doc.DocumentNode.SelectNodes($"{attributePath}//tr[td]");
+            return CreateTableFromHtml(tableHeaders, tableRows);
+        }
+        protected static DataTable CreateTableFromHtml(HtmlNodeCollection headers, HtmlNodeCollection rows)
+        {
+            DataTable table = new DataTable();
+            foreach (HtmlNode header in headers) { table.Columns.Add(header.InnerText); }
+            foreach (var row in rows) { table.Rows.Add(row.SelectNodes("td").Select(td => td.InnerText).ToArray()); }
+            return table;
+        }
     }
 }
